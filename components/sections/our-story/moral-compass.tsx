e9f2c4a3598979c70480d519d04124d4e468513e
Rohan <rohangawande2303@gmail.com>
2025-04-03 15:57:22 +0530

    fix in text size in moral sec

--- components/sections/our-story/moral-compass.tsx
+++ components/sections/our-story/moral-compass.tsx
@@ -33,7 +33,7 @@ export default function MoralCompass() {
         </div>
 
         {/* Who We Are Section - Background Image */}
-        <div className="relative w-full overflow-hidden rounded-lg h-[750px]">
+        <div className="relative w-full overflow-hidden rounded-lg md:h-[500px] h-[750px] pt-0 md:pt-40 bg-[#d2a774]">
           {/* Desktop Background Image */}
           <div className="hidden md:block absolute inset-0 w-full h-full">
             <Image
@@ -59,22 +59,22 @@ export default function MoralCompass() {
           {/* Content Overlay - Positioned in Upper Half Only */}
           <div className="relative z-10 flex flex-col md:flex-row bg-opacity-70 p-8 md:p-12 h-1/2">
             <div className="w-full md:w-1/2 flex flex-col justify-start md:justify-center">
-              <h2 className="text-2xl md:text-3xl font-semibold text-gray-700 mb-2">
+              <h2 className="text-2xl md:text-4xl font-semibold text-gray-700 mb-2">
                 What Makes Us Who We Are
               </h2>
               <div className="w-16 h-1 bg-gray-700 mb-6"></div>
-              <p className="text-gray-700 mb-4">
+              <p className="text-gray-700 mb-4 text-md md:text-xl">
                 Rutu Group has been a successful and innovative frontrunner in
                 the real estate industry for More than 45 years.
               </p>
-              <p className="text-gray-700 mb-4">
+              <p className="text-gray-700 mb-4 text-md md:text-xl">
                 Popularly known and celebrated for its innovative approach, Rutu
                 Group has successfully completed over 45 housing projects across
                 Mumbai, Thane, Kalyan & Ahmedabad, thus providing homes to more
                 than 10,000 families across 10 million sq.ft.
               </p>
               {/* Remove last paragraph in mobile view */}
-              <p className="hidden md:block text-gray-700 mb-4">
+              <p className="hidden md:block text-gray-700 mb-4 text-md md:text-xl">
                 We focus on meticulous planning and strategizing to ensure a
                 timely delivery of high-quality projects that boast of amenities
                 and strives to exceed our customer&apos;s expectations. Our
